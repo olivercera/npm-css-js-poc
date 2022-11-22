09a242a9d008dc1e2bed52f629c1984402371ab3
Oliver Cera <olivercera@gmail.com>
2022-11-22 10:54:37 -0500

    base of library project

---
+++ pages/components/FirstButton.tsx
@@ -0,0 +1,11 @@
+const FirstButton = () => {
+
+    return (
+        <>
+            <button onClick={ () => { alert('yes I am') } }> I am the one! </button>
+        </>
+    )
+
+}
+
+export default FirstButton;
